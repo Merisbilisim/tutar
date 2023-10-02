efd6c3c0312729ffcf19b62b0715388e5cffcdca
Merisbilisim <145693807+Merisbilisim@users.noreply.github.com>
2023-10-02 14:00:41 +0300

    Add files via upload

---
+++ ornek2/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ornek2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBirimFiyat = new System.Windows.Forms.TextBox();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHesepla = new System.Windows.Forms.Button();
+            this.lblToplamFiyat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(26, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Birim Fiyat";
+            // 
+            // txtBirimFiyat
+            // 
+            this.txtBirimFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBirimFiyat.Location = new System.Drawing.Point(139, 88);
+            this.txtBirimFiyat.Name = "txtBirimFiyat";
+            this.txtBirimFiyat.Size = new System.Drawing.Size(120, 29);
+            this.txtBirimFiyat.TabIndex = 1;
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdet.Location = new System.Drawing.Point(139, 133);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(120, 29);
+            this.txtAdet.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(80, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Adet";
+            // 
+            // btnHesepla
+            // 
+            this.btnHesepla.BackColor = System.Drawing.Color.White;
+            this.btnHesepla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesepla.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnHesepla.Location = new System.Drawing.Point(292, 88);
+            this.btnHesepla.Name = "btnHesepla";
+            this.btnHesepla.Size = new System.Drawing.Size(95, 53);
+            this.btnHesepla.TabIndex = 4;
+            this.btnHesepla.Text = "Hesapla";
+            this.btnHesepla.UseVisualStyleBackColor = false;
+            this.btnHesepla.Click += new System.EventHandler(this.btnHesepla_Click);
+            // 
+            // lblToplamFiyat
+            // 
+            this.lblToplamFiyat.AutoSize = true;
+            this.lblToplamFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplamFiyat.Location = new System.Drawing.Point(83, 206);
+            this.lblToplamFiyat.Name = "lblToplamFiyat";
+            this.lblToplamFiyat.Size = new System.Drawing.Size(136, 24);
+            this.lblToplamFiyat.TabIndex = 5;
+            this.lblToplamFiyat.Text = "Toplam Fiyat:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(425, 290);
+            this.Controls.Add(this.lblToplamFiyat);
+            this.Controls.Add(this.btnHesepla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.txtBirimFiyat);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBirimFiyat;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHesepla;
+        private System.Windows.Forms.Label lblToplamFiyat;
+    }
+}
+
---
+++ ornek2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ornek2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesepla_Click(object sender, EventArgs e)
+        {
+            int birimfiyat, adet, toplamfiyat, hesap;
+            
+            birimfiyat=Convert.ToInt32(txtBirimFiyat.Text);
+            adet=Convert.ToInt32(txtAdet.Text);
+            toplamfiyat = birimfiyat * adet;
+
+            lblToplamFiyat.Text="Toplam Fiyat: "+ toplamfiyat;
+
+             
+
+        }
+    }
+}
